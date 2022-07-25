7e8817dba1bfb8f9361583cfcdecfc92c2cfa02f
Manoj SIngh <manoj_singh@bmc.com>
2022-07-25 12:18:35 -0500

    Updated 7-25-1

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
@@ -5,7 +5,7 @@
       ******      C O M P U W A R E   C O R P O R A T I O N       ******
       *                                                                *
       *  THIS PROGRAM IS THE COBOL DEMO PROGRAM USED FOR XPEDITER/TSO  *
-      *  TRAINING SESSIONS. MKS -7-2                                   *
+      *  TRAINING SESSIONS. MKS -7-3                                   *
       *                                                                *
       *  INPUT FILE  - EMPLOYEE WAGE INFORMATION                       *
       *              - STORED IN XPEDITER/TSO SAMPLIB (CWXTDATA)       *
